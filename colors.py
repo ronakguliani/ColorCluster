ed429fe1063150573fd80c08e32acf80a37784b6
Ronak <guliani@pdx.edu>
2023-02-21 16:51:01 -0800

    create repo

--- colors.py
+++ colors.py
@@ -2,29 +2,41 @@ import numpy as np
 from sklearn.cluster import KMeans
 import cv2
 
-# Load the image file
-image = cv2.imread('test1.png')
 
-# Convert the image to a numpy array
-image = np.array(image, dtype=np.uint8)
+def get_top_colors(image_path, num_colors):
+    # Load image and convert from BGR to RGB
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# Reshape the array to a 2D array of pixels
-pixels = image.reshape(-1, 3)
+    # Reshape the image to a 2D array of pixels and 3 color values (RGB)
+    pixels = image.reshape((-1, 3))
 
-# Use KMeans clustering to cluster the pixels in the image into k clusters
-kmeans = KMeans(n_clusters=3, random_state=0).fit(pixels)
+    # Use KMeans clustering to find the most dominant colors
+    kmeans = KMeans(n_clusters=num_colors, random_state=0,
+                    n_init=10).fit(pixels)
 
-# Get the center colors of the k clusters
-center_colors = kmeans.cluster_centers_
+    # Get the colors and sort them by frequency
+    colors = kmeans.cluster_centers_
+    counts = np.bincount(kmeans.labels_)
+    sorted_colors = colors[np.argsort(-counts)]
 
-# Sort the center colors by the number of pixels assigned to each cluster and return the top 3
-counts = np.bincount(kmeans.labels_)
-sorted_counts = np.argsort(counts)[::-1][:3]
-dominant_colors = center_colors[sorted_counts]
+    # Convert the colors from RGB to hex format
+    hex_colors = [RGB_to_HEX(color.astype(int)) for color in sorted_colors]
 
-# Convert the colors to hex values
-dominant_colors_hex = ['#%02x%02x%02x' %
-                       tuple(color.astype(int)) for color in dominant_colors]
+    # Return the top num_colors colors as hex codes
+    return hex_colors[:num_colors]
 
-# Print the result
-print(dominant_colors_hex)
+
+def RGB_to_HEX(color):
+    return '#{:02x}{:02x}{:02x}'.format(*color)
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 3:
+        print("Usage: python colors.py <image_path> <num_colors>")
+    else:
+        image_path = sys.argv[1]
+        num_colors = int(sys.argv[2])
+        top_colors = get_top_colors(image_path, num_colors)
+        print(top_colors)
